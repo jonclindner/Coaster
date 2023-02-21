55d5f27483ec8293de390f880a3251d3c7559f4a
mannyaalonso <mannyaalonso@gmail.com>
2023-02-21 14:05:49 -0500

    nav button route to home instead

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Card from "../components/Card"
 
 const Home = () => {
   return (
-    <div className="bg-slate-700 m-2 rounded-2xl">
+    <div className="bg-slate-800 m-2 rounded-2xl">
       <Card />
     </div>
   )
