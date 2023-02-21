fe42e48fc9847e4e101e7e7778aa3b07a89e46b3
Chikodi Merenu <cmerenu@gmail.com>
2023-02-21 12:43:41 -0600

    backend stup

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router()
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/coaster', controllers.createCoaster)
+
+router.get('/coaster', controllers.getAllCoaster)
+
+router.get('/coaster/:id', controllers.getCoasterById)
+router.put('/coaster/:id', controllers.updateCoaster)
+router.delete('/coaster/:id', controllers.deleteCoaster)
+
+router.post('/coaster/:id/review', controllers.createReview)
+
+router.delete('/review/:id', controllers.deleteReview)
+module.exports = router
